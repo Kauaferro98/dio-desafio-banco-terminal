b4c5219773487e3024718212b9eeefb13ab03aa3
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-21 15:20:18 -0300

    -

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Conta Eletronica");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+        
+        System.out.println("digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("digite o seu nome: ");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+        
+        System.out.println("Olá " + nome +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + 
+        " e seu saldo " + "R$"+ saldo + " já está disponível para saque .");
+        
     }
 }
